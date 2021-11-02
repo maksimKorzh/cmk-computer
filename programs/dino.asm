8a19102a54ea28975bf63c793757147805b58a29
Maksim Korzh <freesoft.for.people@gmail.com>
2021-11-02 12:37:34 +0200

    added DEFINE directive to assembler.py

--- programs/dino.asm
+++ programs/dino.asm
@@ -1,7 +1,3 @@
-
-%define A 0x0000
-;%define B 0x0001
-
 start:                  ; for programs bigger than around 200 bytes
   lpc init              ; make sure to store data in the beginning of the file
                         ; and then jump to program start for otherwise
